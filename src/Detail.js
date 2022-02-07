c4cea81b8cc2ea65a3d28192489ccd986640c36e
sbs1253 <rokaf340@gmail.com>
2022-02-07 16:10:05 +0900

    study

--- src/Detail.js
+++ src/Detail.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from "styled-components"
 import "./Detail.scss"
-
+import { Nav } from 'react-bootstrap';
 let Box =styled.div`
 padding:20px;
 ` ;
@@ -17,7 +17,7 @@ export default function Detail({shoes,재고}){
   let history = useHistory()
   let {id} = useParams();
   const [show,setShow] = useState(true)
-
+  const [누른탭, 누른탭변경]  = useState(0)
   useEffect(()=>{
     let 타이머 = setTimeout(()=>{setShow(false)},2000)
     return ()=>{clearTimeout(타이머)}
@@ -43,8 +43,27 @@ export default function Detail({shoes,재고}){
               <button className="btn btn-danger" onClick={()=>{history.goBack()}}>뒤로가기</button> 
             </div>
           </div>
+
+      <Nav className='mt-5' variant="tabs" defaultActiveKey="link-0">
+        <Nav.Item>
+          <Nav.Link eventKey="link-0" onClick={()=>{누른탭변경(0)}}>Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1" onClick={()=>{누른탭변경(1)}}>Option 2</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent 누른탭={누른탭} />
+      
         </div>  
   )
+}
+function TabContent({누른탭}){
+  if (누른탭===0){
+    return <div>0번째 내용</div>
+  }else if (누른탭===1){
+    return <div>1번째 내용</div>
+  }else
+    return <div>아무내용</div>
 }
 
 function Info({재고}){
